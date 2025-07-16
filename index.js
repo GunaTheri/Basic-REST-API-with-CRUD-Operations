5340f62d678389979de9b2a6181b256d6e4542cc
GunaTheri <gunaravi024@gmail.com>
2025-07-16 10:27:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+import express from "express";
+import bodyParser from "body-parser";
+
+import usersRoutes from './routes/users.js';
+
+const app = express();
+const PORT = 8081;
+
+app.use(bodyParser.json());
+
+app.use('/users', usersRoutes);
+
+app.get('/',(req,res) =>{
+    console.log('[TEST]!');
+    res.send('hello from homepage.');
+});
+app.listen(PORT, () =>console.log(`Server running on port: http://localhost:${PORT}`));
